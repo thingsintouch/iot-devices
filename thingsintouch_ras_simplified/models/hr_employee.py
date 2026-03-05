dd067662eb7d0f89f9d0feb12934a756d4be0c5c
Lu Bu <37246122+lubusax@users.noreply.github.com>
2026-03-05 15:14:06 +0100

    Update copyright year to 2026 in hr_employee.py

--- thingsintouch_ras_simplified/models/hr_employee.py
+++ thingsintouch_ras_simplified/models/hr_employee.py
@@ -1,4 +1,4 @@
-# Copyright 2022 thingsintouch.com
+# Copyright 2026 thingsintouch.com
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from odoo import api, models
 from datetime import datetime, timezone
