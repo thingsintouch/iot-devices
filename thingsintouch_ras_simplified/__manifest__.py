d8dd084a2bc9179ee8caa91bb29f6b94cd4bc6b5
Lu Bu <37246122+lubusax@users.noreply.github.com>
2026-03-05 15:39:35 +0100

    Update copyright year and version in manifest

--- thingsintouch_ras_simplified/__manifest__.py
+++ thingsintouch_ras_simplified/__manifest__.py
@@ -1,9 +1,9 @@
 # Copyright (C) 2018 Creu Blanca
-# Copyright (C) 2022 thingsintouch.com
+# Copyright (C) 2026 thingsintouch.com
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "RAS Simplified Configuration for IoT/OCA",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "category": "IoT",
     "author": "Creu Blanca , "
             "thingsintouch.com",
