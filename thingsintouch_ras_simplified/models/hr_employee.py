144da0e3c3afed6d34e489bd3a07bd13f8a180d0
Luis <lu.bu.sax@gmail.com>
2026-03-05 14:26:33 +0000

    Fix: use naive UTC for attendance sync to prevent errors

--- thingsintouch_ras_simplified/models/hr_employee.py
+++ thingsintouch_ras_simplified/models/hr_employee.py
@@ -1,20 +1,14 @@
-# Copyright 2022 thingsintouch.com
+# Copyright 2026 thingsintouch.com
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 from odoo import api, models
-
-from datetime import datetime
-
+from datetime import datetime, timezone
 import freezegun
-
 
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
-
     @api.model
     def register_attendance_async(self, card_code, timestamp):
-        with freezegun.freeze_time(datetime.fromtimestamp(int(timestamp), tz=None)):
+        frozen_dt = datetime.fromtimestamp(int(timestamp), tz=timezone.utc).replace(tzinfo=None)
+        with freezegun.freeze_time(frozen_dt):
             result = self.register_attendance(card_code)
         return result
-
-
